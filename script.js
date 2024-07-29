23ab92b05a8ca370d7683e4425b68aacfd5111f4
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2024-07-29 13:50:04 -0700

    feat: created operate function for explicit calculation choice

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function operate(firstNum, secondNum, operator) {
+  let result;
+  switch (operator) {
+    case '+':
+      result = add(firstNum, secondNum);
+      break;
+    case '-':
+      result = subtract(firstNum, secondNum);
+      break;
+    case '*':
+      result = multiply(firstNum, secondNum);
+      break;
+    case '/':
+      result = divide(firstNum, secondNum);
+      break;
+    default:
+      console.log('Incorrect input!');
+      break;
+  }
+  return result;
+}
+
 // Calculator display variables
 let firstNum;
 let operator;
@@ -8,3 +30,5 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+console.log(operate(4, 2, '/'));
