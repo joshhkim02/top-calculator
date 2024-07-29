5784e03fbe4521ca89772b36ffb8abee390dd765
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2024-07-29 13:21:48 -0700

    feat: added basic calculator functions and display variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// Calculator display variables
+let firstNum;
+let operator;
+let secondNum;
+
+// Calculator functions
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
