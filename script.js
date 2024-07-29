58715d6af74ba45dc969ba91f25501918bd9a3ec
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2024-07-29 15:47:14 -0700

    feat: number button clicks update display value

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function operate(firstNum, secondNum, operator) {
 let firstNum;
 let operator;
 let secondNum;
+let displayValue;
 
 // Calculator functions
 const add = (num1, num2) => num1 + num2;
@@ -32,3 +33,18 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 // Populate display when number buttons are clicked
+const btnSelector = document.querySelectorAll('.number');
+
+const displayText = document.querySelector('.calc-result');
+
+btnSelector.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (displayText.textContent === '0') {
+      displayText.textContent = item.textContent;
+      displayValue = displayText.textContent;
+    } else {
+      displayText.textContent += item.textContent;
+      displayValue = displayText.textContent;
+    }
+  });
+});
